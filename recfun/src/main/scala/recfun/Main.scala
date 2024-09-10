e530da2712e52177a2772b1a60e77b165dc8f41c
SNOWKIM <rlatnsgh0708@postech.ac.kr>
2024-09-11 02:24:22 +0900

    Modify Main.scala

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,43 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (r == 0) 1
+    else if (c == 0) 1
+    else if (c == r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def inner(chars: List[Char], openCount: Int): Boolean = {
+      if (chars.isEmpty)
+        openCount == 0
+      else if(openCount < 0)
+        false
+      else{
+        if (chars.head == '(')
+          inner(chars.tail, openCount+1)
+        else if (chars.head == ')')
+          inner(chars.tail, openCount-1)
+        else
+          inner(chars.tail, openCount)
+      }
+    }
+    inner(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0 ) 0
+    else if (money == 0) 1
+    else{
+      if (coins.isEmpty) 0
+      else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
 }
